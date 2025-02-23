f3ebe3ee64c71363e0fb6f07a0c892af24097494
Ravi Nayak <nayakrr@mail.uc.edu>
2025-02-23 14:59:54 -0500

    added collectionview

--- ContactList/Contact.cs
+++ ContactList/Contact.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace ContactList
 {
-    internal class Contact
+    public class Contact
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Description { get; set; }
     }
 }
